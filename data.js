c777e36cc03afbb52438c90d33d6b71e8251b204
gbayir68 <gbayir@gmail.com>
2026-02-18 06:42:29 +0300

    Add historical data entries to zamanVerisi

---
+++ data.js
@@ -0,0 +1,7 @@
+const zamanVerisi = {
+    "1990-01-01": "Yeni bir on yılın ilk günü. Türkiye'de kar yağışlı bir sabah... İnsanlar 90'ların neler getireceğinden habersiz.",
+    "1994-04-05": "Ekonomik kararların açıklandığı zor bir gün. Ancak sanat dünyasında o meşhur şarkılar bestelenmeye devam ediyor.",
+    "1997-12-24": "Nartugan bayramı hazırlıkları! Eski Türk geleneklerine göre gece ile gündüzün savaşı başlıyor.",
+    "1999-12-31": "Milenyumun son gecesi. Bilgisayarların kilitleneceği korkusu (Y2K) ve büyük havai fişek gösterileri bir arada.",
+    "VAR_SAYILAN": "Bu tarih zamanın sessiz tanıklarından biri. Büyük bir olay kaydedilmemiş olsa da, o günün insanları için hayat tüm hızıyla akıyordu."
+};
